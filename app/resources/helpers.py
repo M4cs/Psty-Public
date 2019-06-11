02bdbf71a569601fca5460f185274d96f963f1bc
M4cs <mabridgland@protonmail.com>
2019-06-11 16:08:09 -0400

    Add

--- app/resources/helpers.py
+++
@@ -1,6 +0,0 @@
-def generateOptions(dictionary):
-    template = ""
-    for key, value in sorted(dictionary.items()):
-        template += '<option value="{}" name="lang">{}</option>'.format(key, key)
-        template += '\n'
-    return template
